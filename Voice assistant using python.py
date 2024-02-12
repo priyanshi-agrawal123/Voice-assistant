a0805a5e6202005ede4b89fadc07a02c7d63bab9
Priyanshi Agrawal <150780699+priyanshi-agrawal123@users.noreply.github.com>
2024-02-12 03:06:29 -0500

    Update Voice assistant using python.py

--- Voice assistant using python.py
+++ Voice assistant using python.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
 
 import pyttsx3
 import speech_recognition as sr
@@ -49,88 +44,6 @@ if __name__ == '__main__':
         joke_1 = pyjokes.get_joke(language="en",category="neutral")
         print(joke_1)
         speechtx(joke_1)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
 
 
 
